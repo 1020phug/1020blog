52b388d2e6f1ff971c9fd9d065af54fcd6327348
1020phug <1020phug@gmail.com>
2025-04-10 13:16:28 +0700

    Remove junk button at Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,28 +60,10 @@ export default function Home() {
 					/>
 				</motion.div>
 
-				<motion.div
-					initial={{
-						opacity: 0,
-					}}
-					animate={{
-						opacity: 1,
-					}}
-					transition={{
-						duration: 0.3,
-						delay: 1,
-					}}
-					className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
-				>
-					<button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-						<span>Join us</span>
-						<div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-					</button>
-					<button className="px-4 py-2 backdrop-blur-sm border bg-primary border-emerald-500/20 text-primary-foreground mx-auto text-center rounded-full relative mt-4">
-						<span>Contact →</span>
-						<div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-					</button>
-				</motion.div>
+				<button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+					<span>Join us →</span>
+					<div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+				</button>
 			</motion.div>
 		</ImagesSlider>
 	);
